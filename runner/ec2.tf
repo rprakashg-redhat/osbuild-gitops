5e962f8df4ad7c0db0e443d8114e72aa23bab80a
Ram <rgopinat@redhat.com>
2024-05-25 20:21:44 -0400

    testing

---
+++ runner/ec2.tf
@@ -0,0 +1,34 @@
+module "aap" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.8.0"
+
+  name                          = var.instanceName
+  instance_count                = 1
+  
+  instance_type                 = var.instanceType
+  ami                           = var.ami
+  subnet_id                     = tolist(module.vpc.public_subnets)[0]
+  key_name                      = var.sshKey
+  vpc_security_group_ids        = [module.public_subnet_sg.security_group_id]
+  associate_public_ip_address   = true
+  ipv6_addresses = null
+  private_ips = ["10.0.3.142"]
+
+  root_block_device = [
+    {
+      volume_type = "gp2"
+      volume_size = 100
+    },
+  ]
+
+  ebs_block_device = [
+    {
+      device_name = "/dev/sdf"
+      volume_type = "gp2"
+      volume_size = 50
+      encrypted   = false
+    }
+  ]
+
+  tags = local.tags
+}
---
+++ runner/variables.tf
@@ -0,0 +1,51 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "stack" {
+  description = "Name of Stack"
+  type        = string
+  default     = "github-runners"
+}
+
+variable instanceName {
+  description = "EC2 instance name"
+  type        = string
+  default     = "rhel9-builder"
+}
+
+variable "instanceType" {
+  description = "EC2 instance type to use"  
+  type = string
+  default = "m5.xlarge"
+}
+
+variable sshKey {
+  description = "SSH key pair name"
+  type        = string
+  default     = "ec2"
+}
+
+variable "ami" {
+  description = "AMI to use"
+  type = string
+  default = "ami-0f7197c592205b389"
+}
+
+variable "myip" {
+  description = "Public IP assigned by my ISP"
+  type        = string
+  default     = "71.67.107.138/32"
+}
+
+variable rhUser {
+  description = "Red Hat user account"
+  type        = string
+}
+
+variable rhUserPassword {
+  description = "Red Hat user account password"
+  type        = string
+}
